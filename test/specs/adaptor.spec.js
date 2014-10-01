7eb186398414f8061f30f0dc4e2b7ee6a606e2ca
deadprogram <ron@hybridgroup.com>
2014-10-01 13:40:36 -0700

    Initial commit

---
+++ test/specs/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var WicedSense = source("adaptor");
+
+describe("Cylon.Adaptors.WicedSense", function() {
+  var adaptor = new WicedSense();
+
+  it("needs tests");
+});
---
+++ test/specs/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var WicedSense = source("driver");
+
+describe("Cylon.Drivers.WicedSense", function() {
+  var driver = new WicedSense({
+    device: { connection: 'connect' }
+  });
+
+  it("needs tests");
+});
