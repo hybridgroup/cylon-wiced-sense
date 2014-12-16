f254af5969566977cc2f3a9fb9eec2193ea3675c
Andrew Stewart <andrew@stwrt.ca>
2014-12-16 09:41:55 -0800

    Update examples

---
+++ examples/wiced-sense/wiced-sense.js
@@ -0,0 +1,41 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    bluetooth: { adaptor: 'ble', uuid: '207377654321' }
+  },
+
+  devices: {
+    battery: { driver: 'ble-battery-service' },
+    deviceInfo: { driver: 'ble-device-information' },
+    generic: { driver: 'ble-generic-access' },
+    wiced: { driver: 'wiced-sense' }
+  },
+
+  display: function(err, data) {
+    if (!!err) {
+      console.log("error:", err);
+      return;
+    }
+
+    console.log("data:", data);
+  },
+
+  work: function(my) {
+    my.generic.getDeviceName(function(err, data) {
+      my.display(err, data);
+
+      my.generic.getAppearance(function(err, data) {
+        my.display(err, data);
+
+        my.deviceInfo.getManufacturerName(function(err, data) {
+          my.display(err, data);
+
+          my.wiced.getData(function(err, data) {
+            my.display(err, data);
+          });
+        });
+      });
+    });
+  }
+}).start();
